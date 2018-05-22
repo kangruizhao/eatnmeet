ff704127760e4b47f412b18b1c4caa280d1c5a0f
Kangrui Zhao <kzhao11@hawk.iit.edu>
2018-05-21 20:36:05 -0500

    added Procfileaasdadasd

--- routing.py
+++ routing.py
@@ -195,5 +195,6 @@ def searchLocation(location,meat):
     restaurant_info=findARestaurant(meat,location)
     return restaurant_info['name']
 if __name__ == '__main__':
+   port = int(os.environ.get('PORT', 5000))
    app.run(host='0.0.0.0', port=port,debug=True)
    #app.run('127.0.0.1',8000, debug=True)
